b6d37fd460b19e0470be9b1989496c81152c2335
MatheusLimaby <matheus.l.rodrigues@iesb.edu.br>
2026-04-26 21:37:33 -0300

    feat: 37

--- praticas/chronos-pomodoro/src/App.tsx
+++ praticas/chronos-pomodoro/src/App.tsx
@@ -1,10 +1,30 @@
 import { Home } from './pages/Home';
-//import { NotFound } from './pages/NotFound';
-//import { AboutPomodoro } from './pages/AboutPomodoro';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { useState } from 'react';
+import type { TaskStateModel } from './models/TaskStateModel';
+import { TaskContextProvider } from './contexts/TaskContext';
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 
 export function App() {
-  return <Home />;
+  const [state, setState] = useState(initialState);
+
+  return (
+    // Usamos o nosso componente limpo e encapsulado
+    <TaskContextProvider>
+      <Home />
+    </TaskContextProvider>
+  );
 }
--- praticas/chronos-pomodoro/src/components/MainForm/index.tsx
+++ praticas/chronos-pomodoro/src/components/MainForm/index.tsx
@@ -2,10 +2,34 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
+import { useTaskContext } from '../../contexts/TaskContext';
 
 export function MainForm() {
+  // Puxamos a função 'setState' do contexto
+  const { setState } = useTaskContext();
+
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log('DEU CERTO');
+  }
+
+  // Função de teste para atualizar o estado global
+  function handleClick() {
+    setState(prevState => {
+      return {
+        ...prevState,
+        formattedSecondsRemaining: '21:00', // Força o relógio a mostrar 21:00
+      };
+    });
+  }
+
   return (
-    <form className='form' action=''>
+    <form onSubmit={handleCreateNewTask} className='form' action=''>
+      {/* Botão de teste */}
+      <button onClick={handleClick} type='button'>
+        Clicar
+      </button>
+
       <div className='formRow'>
         <DefaultInput
           labelText='task'
@@ -16,7 +40,7 @@ export function MainForm() {
       </div>
 
       <div className='formRow'>
-        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Próximo intervalo é de 25min</p>
       </div>
 
       <div className='formRow'>
