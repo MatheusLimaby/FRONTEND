10b37be2a38560a9ea99d7eaa3fbe45295c7df18
MatheusLimaby <matheus.l.rodrigues@iesb.edu.br>
2026-04-29 09:41:38 -0300

    feat: Login

--- praticas/pomodoro/src/App.tsx
+++ praticas/pomodoro/src/App.tsx
@@ -1,4 +1,5 @@
 import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
+import { AuthContextProvider } from './contexts/AuthContext'; // 👈 FALTAVA ISSO
 import { MessagesContainer } from './components/MessagesContainer';
 import { MainRouter } from './routers/MainRouter';
 import './styles/theme.css';
@@ -7,9 +8,11 @@ import './styles/global.css';
 export function App() {
   return (
     <TaskContextProvider>
-      <MessagesContainer>
-        <MainRouter />
-      </MessagesContainer>
+      <AuthContextProvider> 
+        <MessagesContainer>
+          <MainRouter />
+        </MessagesContainer>
+      </AuthContextProvider>
     </TaskContextProvider>
   );
 }
--- praticas/pomodoro/src/routers/MainRouter/index.tsx
+++ praticas/pomodoro/src/routers/MainRouter/index.tsx
@@ -5,6 +5,9 @@ import { Home } from '../../pages/Home';
 import { useEffect } from 'react';
 import { History } from '../../pages/History';
 import { Settings } from '../../pages/Settings';
+import { ProtectedRoute } from '../../components/ProtectedRoute';
+import { PublicOnlyRoute } from '../../components/PublicOnlyRoute';
+import { Login } from '../../pages/Login';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -20,11 +23,15 @@ export function MainRouter() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<PublicOnlyRoute><Login /></PublicOnlyRoute>} />
+        <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+        <Route path="/history/" element={<ProtectedRoute><History /></ProtectedRoute>} />
         <Route path='/' element={<Home />} />
         <Route path='/history/' element={<History />} />
         <Route path='/settings/' element={<Settings />} />
         <Route path='/about-pomodoro/' element={<AboutPomodoro />} />
         <Route path='*' element={<NotFound />} />
+
       </Routes>
       <ScrollToTop />
     </BrowserRouter>
